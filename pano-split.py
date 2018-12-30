a6085b27963ff97cff8fc3311888afd0e166188e
Brandon Temple <brandon.temple.97@gmail.com>
2018-12-30 15:12:58 -0800

    worked out the bugs, all should be a go!

--- pano-split.py
+++ pano-split.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 import sys
 
 # Check args
@@ -9,11 +10,7 @@ if len(sys.argv) != 3:
 pano = cv2.imread(sys.argv[1])
 pano_height, pano_width, _ = pano.shape
 num_splits = int(sys.argv[2])
-split_offset = (pano_width % pano_height) / num_splits
-split_width = pano_height + split_offset
-
+split_width = int(pano_width/num_splits)
+splits = np.split(pano,3,1)
 for i in range(num_splits):
-    print('Splitting %s into split_%d'%(sys.argv[1], i))
-    start = int(i*split_width)
-    end = int((i+1)*split_width)
-    cv2.imwrite('split_%d'%i, pano[start:end])
+    cv2.imwrite('split_%d.jpg'%i, splits[i])
